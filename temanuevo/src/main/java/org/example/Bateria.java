97baa7a33321a02a1a657d247ccc55a20067be9f
A604\1DAW <edgarfer2205@gmail.com>
2024-10-11 15:24:12 +0200

    .

--- temanuevo/src/main/java/org/example/Bateria.java
+++ temanuevo/src/main/java/org/example/Bateria.java
@@ -6,11 +6,22 @@ public class Bateria {
     public void ejercicio1(){
         Scanner entrada = new Scanner(System.in);
         System.out.println("Dime una frase");
-        String frase = entrada.next();
+        String frase = entrada.nextLine();
 
         System.out.println("Frase original " + frase);
 
         int longitud = frase.length();
         System.out.println("El tamaño de la frase es " + longitud);
+
+        String reemplazar_espacios = frase.replace(" ","");
+        System.out.println("La frase sin espacios es " + reemplazar_espacios);
+
+        int mitad = longitud / 2;
+        String mitad1 = frase.substring(0, mitad);
+        String mitad2 = frase.substring(mitad);
+        System.out.println("Primera mitad del texto es ´" + mitad1 + "´ y la segunda mitad del texto es ´" + mitad2 + "´");
+
+        String mayusculas = frase.toUpperCase();
+        System.out.println("La frase en mayusculas es " + mayusculas);
     }
 }
