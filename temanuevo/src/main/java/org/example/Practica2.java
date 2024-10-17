d3e678a0a1554b5c5e721cfdd6cc79393c06955d
A604\1DAW <edgarfer2205@gmail.com>
2024-10-17 20:51:35 +0200

    .

--- temanuevo/src/main/java/org/example/Practica2.java
+++ temanuevo/src/main/java/org/example/Practica2.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Practica2 {
@@ -7,23 +8,17 @@ public class Practica2 {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Dime tu primer numero de la operacion: ");
-
         int num1 = teclado.nextInt();
 
-        System.out.println("Dime la operacion que quieres: ");
 
-        String operacion = teclado.next();
 
-        String raiz = "r";
-
-        if (operacion == raiz) {
-            int resultado11 = num1 * num1;
-            System.out.println("El resultado es: " + resultado11);
-        } else {
-
-            System.out.println("Dime tu segundo numero para hacer la operacion: ");
-            int num2 = teclado.nextInt();
-
+            System.out.println("Dime la operacion que quieres: ");
+            String operacion = teclado.next();
+            int num2= 0;
+            if (!operacion.equalsIgnoreCase("r")) {
+                System.out.println("Dime tu segundo numero para hacer la operacion: ");
+                num2 = teclado.nextInt();
+            }
             switch (operacion) {
                 case ("+"):
                     int resultado = num1 + num2;
@@ -34,12 +29,23 @@ public class Practica2 {
                     System.out.println("El resultado es " + resultado2);
                     break;
                 case ("/"):
-                    int resultado3 = num1 / num2;
+                    int numero23 = 0;
+                    if (num2 == 0) {
+                        System.out.println("Error 0 NO VALE");
+                    }
+                    System.out.println("Dime un numero valido");
+                    numero23 = teclado.nextInt();
+                    int resultado3 = num1 / numero23;
                     System.out.println("El resultado es " + resultado3);
+
                     break;
                 case ("*"):
                     int resultado4 = num1 * num2;
                     System.out.println("El resultado es " + resultado4);
+                    break;
+                case ("r"):
+                    int resultado5 = num1 * num1;
+                    System.out.println("El resultado es: " + resultado5);
                     break;
                 default:
                     System.out.println("Esa operacion no esta en esta calculadora");
@@ -47,4 +53,4 @@ public class Practica2 {
             }
         }
     }
-}
+
