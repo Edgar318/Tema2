a880e6a49738842db13678f5559df36826a6cb99
Edgar318 <edgarfer2205@gmail.com>
2024-10-24 12:50:15 +0200

    Update Practica3.java

--- temanuevo/src/main/java/org/example/Practica3.java
+++ temanuevo/src/main/java/org/example/Practica3.java
@@ -5,20 +5,36 @@ import java.util.Scanner;
 public class Practica3 {
     public void ibsn(){
         Scanner teclado = new Scanner(System.in);
-
-        System.out.println("Dime el IBSN que quieras comprobar: ");
-
-        String numero = teclado.next();
-        int sum = 0 ;
-        for (int i = 0; i < 10;i++){
-            int digito = numero.charAt(i);
-            sum += (10 - i) * digito;
+        String numero;
+        while (true) {
+            System.out.println("Dime el ISBN que quieres comprobar: ");
+            numero = teclado.next();
+            if (numero.length() == 10){
+                break;
+            }else{
+                System.out.println("El ISBN esta mal.")
+            } 
         }
-        if (sum%11==0){
-            System.out.println("El " + numero + " es valido.");
+
+        int sum = 0;
+        for (int = 0 ; i < 9; i++){
+        int digito = Character.getNumericValue(numero.charAt(i))
+        sum += (10 - i) * digito;
+        }
+
+        char digito2 = numero.charAt(9);
+        if (digito2 == "X" || digito2 == "x"){
+            sum += 10;
         }else{
-            System.out.println("El " + numero + " no es valido.");
-        }
+            int digito = Character.getNumericValue(digito2);
+            sum += digito ;
+        
+        }    
 
+        if (sum % 11 == 0){
+                System.out.println("El " + numero + " es valido.")
+        }else{
+                System.out.println("El " + numero + " es valido.")
+        }      
     }
 }
