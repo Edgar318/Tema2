9c2550c5665d9bac6244e37c2f56315f9f3aa23f
A604\1DAW <edgarfer2205@gmail.com>
2024-10-30 21:12:33 +0100

    .

--- temanuevo/src/main/java/org/example/Main.java
+++ temanuevo/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-            examenT2 ejercicio2= new examenT2();
-            ejercicio2.ejercicio2();
+            examenT2 ejercicio1 = new examenT2();
+            ejercicio1.ejercicio1();
         }
     }
